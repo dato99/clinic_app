b5c01b97af84c86ab32e1dc0ac589305ec792cf6
davit.shengelia <davit.shengelia@D-SHENGELIA>
2024-12-17 11:56:39 +0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { CategoryComponent } from './components/category/category.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { DoctorListComponent } from './components/doctor-list/doctor-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroSectionComponent,
+    CategoryComponent,
+    DoctorListComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
