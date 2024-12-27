ac41191b2f98cf67d12f461261a99e2aba631fb8
davit.shengelia <davit.shengelia@D-SHENGELIA>
2024-12-27 13:47:42 +0400

    changes

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -14,31 +14,25 @@ import { Calendar } from '@fullcalendar/core';
 export class BookingComponent {
   @ViewChild('calendar') calendar: any;
 
-
-
   events: any[] = [];
-
+  isBookingModalOpen: boolean = false;
+  selectedTime: string = '';
+  selectedDate: string = '';
 
   calendarOptions = {
+    plugins: [dayGridPlugin, interactionPlugin], 
     initialView: 'dayGridMonth',
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
       right: 'dayGridMonth,dayGridWeek,dayGridDay',
     },
-    events: this.events, 
+    events: this.events,
     editable: true,
-    droppable: true,
     selectable: true,
     select: this.handleDateSelect.bind(this),
     eventClick: this.handleEventClick.bind(this),
-    
   };
-
-
-  isBookingModalOpen: boolean = false;
-  selectedTime: string = ''; 
-  selectedDate: string = ''; 
 
   constructor(private http: HttpClient) {}
 
@@ -47,72 +41,83 @@ export class BookingComponent {
   }
 
   fetchEvents() {
-    this.http.get<any[]>('http://localhost:5000/api/appointments') 
-      .subscribe((data) => {
-        this.events = data.map(event => ({
+    this.http.get<any[]>('http://localhost:5000/api/appointments').subscribe(
+      (data) => {
+        this.events = data.map((event) => ({
           title: event.title,
           start: event.start,
           end: event.end,
           id: event.id,
           allDay: event.allDay || false,
         }));
-      }, (error) => {
-        console.error('Error fetching events:', error);
-      });
+        this.calendarOptions.events = this.events; 
+      },
+      (error) => console.error('Error fetching events:', error)
+    );
   }
 
   handleDateSelect(selectInfo: any) {
-    const selectedDate = selectInfo.startStr;
-    this.selectedDate = selectedDate; 
-    this.isBookingModalOpen = true; 
+    this.selectedDate = selectInfo.startStr;
+    this.isBookingModalOpen = true;
   }
 
   handleEventClick(clickInfo: any) {
-    const updatedTime = prompt('Update your appointment time (e.g., 12:00 PM):');
     
+    const updatedTime = prompt('Update your appointment time (e.g., 12:00 PM):');
+  
     if (updatedTime) {
       const updatedEvent = {
-        ...clickInfo.event,
-        start: `${clickInfo.event.startStr.split('T')[0]}T${updatedTime}:00`,
-        end: `${clickInfo.event.startStr.split('T')[0]}T${updatedTime}:59`
+        ...clickInfo.event, 
+        start: `${clickInfo.event.startStr.split('T')[0]}T${updatedTime}:00`, 
+        end: `${clickInfo.event.startStr.split('T')[0]}T${updatedTime}:59` 
       };
+  
 
-      this.http.put('http://localhost:5000/api/appointments/' + clickInfo.event.id, updatedEvent)
-        .subscribe(response => {
-          alert('Your appointment has been updated!');
-        }, error => {
-          alert('There was an error updating your appointment.');
-        });
-
-      clickInfo.event.setStart(updatedEvent.start);
-      clickInfo.event.setEnd(updatedEvent.end);
+      this.http.put(`http://localhost:5000/api/appointments/${clickInfo.event.id}`, updatedEvent)
+        .subscribe(
+          response => {
+            alert('Your appointment has been updated!');
+          
+            clickInfo.event.setStart(updatedEvent.start);
+            clickInfo.event.setEnd(updatedEvent.end);
+          },
+          error => {
+            alert('There was an error updating your appointment.');
+            console.error(error);
+          }
+        );
     }
   }
-
+  
 
   bookAppointment() {
+    
     const newEvent = {
-      title: 'Booked Appointment',
+      title: 'Booked Appointment', 
       start: `${this.selectedDate}T${this.selectedTime}:00`,
-      end: `${this.selectedDate}T${this.selectedTime}:59`,
-      allDay: false
+      end: `${this.selectedDate}T${this.selectedTime}:59`, 
+      allDay: false 
     };
+  
 
-   
     this.http.post('http://localhost:5000/api/appointments', newEvent)
-      .subscribe(response => {
-        alert('Your appointment has been booked!');
-        this.events.push(newEvent); 
-        this.isBookingModalOpen = false; 
-      }, error => {
-        alert('There was an error booking your appointment.');
-      });
+      .subscribe(
+        response => {
+          alert('Your appointment has been booked!');
+          this.events.push(newEvent);
+          this.isBookingModalOpen = false; 
+        },
+        error => {
+          alert('There was an error booking your appointment.');
+          console.error(error);
+        }
+      );
   }
-
 
   closeBookingModal() {
-    this.isBookingModalOpen = false; 
+   
+    this.isBookingModalOpen = false;
+    this.selectedTime = ''; 
   }
 }
-  
 
